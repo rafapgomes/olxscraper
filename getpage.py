17d279eb0bb466b788f3397862455f7cc49cdc4b
rafapgomes <pimentelgomes17@gmail.com>
2021-04-16 10:59:19 -0300

    update getpage.py

--- getpage.py
+++ getpage.py
@@ -1,11 +1,14 @@
 import requests
 
-
 headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
     }
 
-def request():
-    return requests.get("https://www.olx.com.br/brasil?q=notebook%20ryzen%203",headers=headers)
+
+def request(link):
+    return requests.get(link,headers=headers)
+
+
+
 
 
