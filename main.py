ea701762e4eb589a0ae4bc44b0da27dd10ac8547
rafapgomes <pimentelgomes17@gmail.com>
2021-04-19 15:05:11 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import getpage
 import scrapepage
 import arq
-link = "https://www.olx.com.br/brasil?o=1&q=notebook%20ryzen%203"
+
+item = "notebook ryzen 3"
+link = getpage.getlink(item)
+
 page = getpage.request(link)
 
 while(scrapepage.listpage(page)!=0):
+
     lista_anuncios = scrapepage.getadlist(page)
     anuncios_tratados = scrapepage.trataanucio(lista_anuncios)
     vet = []
-    vet = scrapepage.getanunc(anuncios_tratados)
+    vet = scrapepage.getanunc(anuncios_tratados,"NOTEBOOK")
     arq.writearq(vet)
     link = scrapepage.listpage(page)
     print(link)
+
     page = getpage.request(link)
     print("-------------------------------------------------------------")
 
